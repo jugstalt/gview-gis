1f03d78759ea9b0a19c496e5f06beacda6e5a783
jugstalt <gstalt@gmx.at>
2018-11-01 12:32:41 +0100

    Nuget Updates

--- gView.Server/Startup.cs
+++ gView.Server/Startup.cs
@@ -175,8 +175,8 @@ namespace gView.Server
             });
 
             PlugInManager.Init();
-            InternetMapServer.Init(@"C:\ProgramData\gView\mapServer\Services\8001");
-            //InternetMapServer.Init(@"C:\Development_OpenSource\GeoDaten\MXL\8050");
+            //InternetMapServer.Init(@"C:\ProgramData\gView\mapServer\Services\8001");
+            InternetMapServer.Init(@"C:\Development_OpenSource\GeoDaten\MXL\8050");
         }
     }
 }
