2377e37a7d40403499ff2caf44f4d09a13d70501
jugstalt <gstalt@gmx.at>
2020-03-20 07:36:12 +0100

    test

--- gView.Server/Program.cs
+++ gView.Server/Program.cs
@@ -18,6 +18,7 @@ namespace gView.Server
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
