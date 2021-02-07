63a3f198ebb405404d4a4b72016430d8ed93def8
jugstalt <gstalt@gmx.at>
2021-02-07 16:29:55 +0100

    feat: MapResourcePicker

---
+++ NetFramework/gView.Win.Symbology.UI/Framework/Dialogs/FormMapResourcePicker.cs
@@ -0,0 +1,58 @@
+﻿using gView.Framework.Carto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gView.Framework.UI.Symbology.Dialogs
+{
+    public partial class FormMapResourcePicker : Form
+    {
+        private readonly IMap _currentMap;
+
+        public FormMapResourcePicker(IMap currentMap)
+        {
+            _currentMap = currentMap;
+
+            InitializeComponent();
+
+            BuildList();
+        }
+
+        void BuildList()
+        {
+            lstResources.Items.Clear();
+
+            if (_currentMap?.ResourceContainer != null)
+            {
+                foreach (var resourceName in _currentMap.ResourceContainer.Names)
+                {
+                    lstResources.Items.Add(resourceName);
+                }
+            }
+        }
+
+        private void lstResources_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnOK.Enabled = (lstResources.SelectedItem != null);
+        }
+
+        public void SetAbortButtonText(string text)
+        {
+            btnAbort.Text = text;
+        }
+
+        public string SelectedResourceName
+        {
+            get
+            {
+                return lstResources.SelectedItem?.ToString();
+            }
+        }
+    }
+}
--- gView.System/Framework/System/SystemInfo.cs
+++ gView.System/Framework/System/SystemInfo.cs
@@ -9,7 +9,7 @@ namespace gView.Framework.system
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(5, 0, 19);
+        public static Version Version = new Version(5, 0, 20);
 
         #region -> Private Variables
 
