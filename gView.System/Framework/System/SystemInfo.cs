5c081c5dd2a419a2217a5a914a07ddd230dad241
jugstalt <gstalt@gmx.at>
2021-04-19 18:40:04 +0200

    commit

--- gView.System/Framework/System/SystemInfo.cs
+++ gView.System/Framework/System/SystemInfo.cs
@@ -9,7 +9,7 @@ namespace gView.Framework.system
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(5, 21, 1501);
+        public static Version Version = new Version(5, 21, 1601);
 
         #region -> Private Variables
 
