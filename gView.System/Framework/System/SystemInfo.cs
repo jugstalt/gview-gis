ecc51bb4bacc06ee73cce754141595ecebdbabc1
jugstalt <gstalt@gmx.at>
2021-06-14 16:56:43 +0200

    commit

--- gView.System/Framework/System/SystemInfo.cs
+++ gView.System/Framework/System/SystemInfo.cs
@@ -9,7 +9,7 @@ namespace gView.Framework.system
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(5, 21, 1801);
+        public static Version Version = new Version(5, 21, 2201);
 
         #region -> Private Variables
 
@@ -26,7 +26,7 @@ namespace gView.Framework.system
 
         #endregion
 
-        internal static string MashineName
+        internal static string MachineName
         {
             get
             {
