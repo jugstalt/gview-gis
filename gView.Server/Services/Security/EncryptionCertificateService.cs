4f3a48b6710b1ed6452623d7c61f53e187df0296
jugstalt <gstalt@gmx.at>
2022-02-04 12:41:41 +0100

    ReloadCert

--- gView.Server/Services/Security/EncryptionCertificateService.cs
+++ gView.Server/Services/Security/EncryptionCertificateService.cs
@@ -2,12 +2,9 @@
 using gView.Framework.Security;
 using gView.Server.Services.MapServer;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
 namespace gView.Server.Services.Security
 {
