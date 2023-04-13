c1d15585614205e36c12c0c92b8257ce7c410e78
jugstalt <gstalt@gmx.at>
2023-04-13 07:13:48 +0200

    Version Update

--- gView.System/Framework/System/SystemInfo.cs
+++ gView.System/Framework/System/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.system
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(5, 23, 1503);
+        public static Version Version = new Version(5, 23, 1505);
 
         #region -> Private Variables
 
