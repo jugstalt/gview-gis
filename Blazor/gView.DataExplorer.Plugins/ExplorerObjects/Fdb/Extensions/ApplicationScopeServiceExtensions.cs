297e78783deba15ae4ddb3cc31b1bc00e197f8ea
Jürgen Gstaltmaier <juergen.gstaltmaier@e-netze.at>
2023-07-19 14:47:02 +0200

    sync

--- Blazor/gView.DataExplorer.Plugins/ExplorerObjects/Fdb/Extensions/ApplicationScopeServiceExtensions.cs
+++ Blazor/gView.DataExplorer.Plugins/ExplorerObjects/Fdb/Extensions/ApplicationScopeServiceExtensions.cs
@@ -1,14 +1,21 @@
 ﻿using gView.Cmd.Core.Abstraction;
 using gView.Cmd.Core.Models;
 using gView.Cmd.Fdb.Lib;
+using gView.DataExplorer.Plugins.ExplorerObjects.Fdb.MsSql;
+using gView.DataExplorer.Plugins.ExplorerObjects.Fdb.PostgreSql;
+using gView.DataExplorer.Plugins.ExplorerObjects.Fdb.SqLite;
 using gView.DataExplorer.Plugins.Services;
 using gView.DataExplorer.Razor.Components.Dialogs.Models;
+using gView.DataSources.Fdb.MSAccess;
+using gView.DataSources.Fdb.MSSql;
+using gView.DataSources.Fdb.PostgreSql;
 using gView.DataSources.Fdb.SQLite;
 using gView.Framework.Blazor;
 using gView.Framework.Data;
 using gView.Framework.DataExplorer.Abstraction;
 using gView.Framework.system;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -93,13 +100,46 @@ static internal class ApplicationScopeServiceExtensions
             return null;
         }
 
-        var instance = 
+        AccessFDB? fdb = null;
+        bool opened;
+        switch(parentExObject)
+        {
+            case SqlFdbExplorerObject sqlExObject:
+                fdb = new SqlFDB();
+                opened = await fdb.Open(sqlExObject.ConnectionString);
+                break;
+            case PostgreSqlExplorerObject pgExObject:
+                fdb = new pgFDB();
+                opened = await fdb.Open(pgExObject.ConnectionString);
+                break;
+            case SqLiteFdbExplorerObject sqliteExObject:
+                fdb = new SQLiteFDB();
+                opened = await fdb.Open(sqliteExObject.FullName);
+                break;
+            default:
+                throw new Exception("Unknown FDB Engine");
+        }
 
+        if (!opened)
+        {
+            throw new Exception(fdb.LastErrorMessage);
+        }
+        
         ICommand command = new CreateFeatureClassCommand();  // ToDo: CreateDatasetCommand();
         var parameters = new Dictionary<string, object>()
         {
-
+            { "fdb", fdb.GetType().Name },
+            { "connection_string", fdb.ConnectionString },
+            { "ds_name", model.Name },
+            { "ds_type", model.DatasetType.ToString() }
         };
+
+        if (model.SpatialReference != null)
+        {
+            parameters.Add("sref", model.SpatialReference.EpsgCode);
+        }
+
+        if(model.DatasetType == NewFdbDatasetType)
 
         return null;
     }
