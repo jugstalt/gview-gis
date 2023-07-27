f0278745492d8bc1edb6938ab21d4cd41dbeda7b
jugstalt <gstalt@gmx.at>
2023-07-27 20:44:45 +0200

    FillLuceneServer: Changed "category" from FieldTypes.StringType to FieldTypes.TextType

--- gView.System/Framework/System/SystemInfo.cs
+++ gView.System/Framework/System/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.system
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(5, 23, 2101);
+        public static Version Version = new Version(5, 23, 3001);
 
         #region -> Private Variables
 
