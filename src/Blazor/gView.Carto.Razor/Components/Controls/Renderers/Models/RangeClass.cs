6126a5f93c3930a772e1f59098739fd83da36ebd
jugstalt <gstalt@gmx.at>
2023-12-08 17:21:02 +0100

    Carto: QuantityRenderer

--- src/Blazor/gView.Carto.Razor/Components/Controls/Renderers/Models/RangeClass.cs
+++ src/Blazor/gView.Carto.Razor/Components/Controls/Renderers/Models/RangeClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace gView.Carto.Razor.Components.Controls.Renderers.Models;
+﻿namespace gView.Carto.Razor.Components.Controls.Renderers.Models;
 internal class RangeClass
 {
     public RangeClass(double min, double max)
---
+++ src/Blazor/gView.Carto.Razor/Extensions/ArgbColorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using gView.GraphicsEngine;
+
+namespace gView.Carto.Razor.Extensions;
+internal static class ArgbColorExtensions
+{
+    static public ArgbColor AlterTo(this ArgbColor from, ArgbColor to, float fac)
+    {
+        if (fac < 0.0f)
+        {
+            return from;
+        }
+
+        if (fac > 1.0f)
+        {
+            return to;
+        }
+
+        try
+        {
+            return ArgbColor.FromArgb(
+                from.A + (int)((double)(to.A - from.A) * fac),
+                from.R + (int)((double)(to.R - from.R) * fac),
+                from.G + (int)((double)(to.G - from.G) * fac),
+                from.B + (int)((double)(to.B - from.B) * fac));
+        }
+        catch
+        {
+            return ArgbColor.Transparent;
+        }
+    }
+}
