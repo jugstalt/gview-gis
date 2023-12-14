4d64225bdad7e6866f649e894e4e3f843638acd3
jugstalt <gstalt@gmx.at>
2023-12-14 08:05:52 +0100

    Carto: UniversalGeometryRendererProepertyPage

---
+++ src/Blazor/gView.Carto.Plugins/PropertyPages/Renderers/UniversalGeometryRendererPropertyPage.cs
@@ -0,0 +1,16 @@
+﻿using gView.Framework.Carto.Abstraction;
+using gView.Framework.Cartography.Rendering;
+using gView.Framework.Core.Carto;
+using gView.Framework.Core.Common;
+
+namespace gView.Carto.Plugins.PropertyPages.Renderers;
+
+[RegisterPlugIn("0C85962C-C445-481B-9B5F-EC124F78D961")]
+internal class UniversalGeometryRendererPropertyPage : IPropertyPageDefinition
+{
+    public Type InterfaceType => typeof(IFeatureRenderer);
+
+    public Type InstanceType => typeof(UniversalGeometryRenderer);
+
+    public Type PropertyPageType => typeof(Razor.Components.Controls.Renderers.UniversalGeometryRendererPropertyPage);
+}
