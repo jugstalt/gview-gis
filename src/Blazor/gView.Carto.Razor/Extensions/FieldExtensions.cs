c1e35ab052181a9612b7b86eeef701160211e73f
jugstalt <gstalt@gmx.at>
2023-12-22 21:57:01 +0100

    sync

---
+++ src/Blazor/gView.Carto.Razor/Extensions/FieldExtensions.cs
@@ -0,0 +1,31 @@
+﻿using gView.Framework.Core.Data;
+
+namespace gView.Carto.Razor.Extensions;
+
+static internal class FieldExtensions
+{
+    static public string FieldValueFormatString(this IField field)
+        => field.type switch
+        {
+            FieldType.Date => "'{0}'",
+            FieldType.character => "'{0}'",
+            FieldType.guid => "'{0}'",
+            FieldType.NString => "'{0}'",
+            FieldType.String => "'{0}'",
+            _ => "{0}"
+        };
+
+    static public string FieldWhereClauseSegment(this IField field, string value)
+    {
+        var formatString = field.FieldValueFormatString();
+
+        var queryOperator = field.type switch
+        {
+            FieldType.String /*when value.Contains("%")*/ => " like ",  // aways like... also solves case insensiv
+            FieldType.NString /*when value.Contains("%")*/ => " like ",
+            _ => "="
+        };
+
+        return $"{field.name}{queryOperator}{String.Format(formatString, value)}";
+    }
+}
