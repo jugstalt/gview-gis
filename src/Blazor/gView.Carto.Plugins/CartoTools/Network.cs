969d8b90335756a2314af0fca669fb2a18d73996
jugstalt <gstalt@gmx.at>
2024-01-24 21:27:52 +0100

    sync

---
+++ src/Blazor/gView.Carto.Plugins/CartoTools/Network.cs
@@ -0,0 +1,48 @@
+﻿using gView.Carto.Core;
+using gView.Carto.Core.Models.MapEvents;
+using gView.Carto.Core.Services.Abstraction;
+using gView.Framework.Carto.Abstraction;
+using gView.Framework.Core.Common;
+
+namespace gView.Carto.Plugins.CartoTools;
+
+[RegisterPlugIn("8F2B6C49-D24A-4017-9554-AD15F54A1B06")]
+internal class Network : ICartoTool
+{
+    #region ICartoButton
+
+    public string Name => "Network";
+
+    public string ToolTip => "Network Tools";
+
+    public ToolType ToolType => ToolType.Click;
+
+    public string Icon => "webgis:construct-edge-intersect";
+
+    public CartoToolTarget Target => CartoToolTarget.Tools;
+
+    public int SortOrder => 10;
+
+    public void Dispose()
+    {
+
+    }
+
+    public bool IsEnabled(ICartoApplicationScopeService scope) => true;
+
+    public Task<bool> OnClick(ICartoApplicationScopeService scope)
+        => Task.FromResult(true);
+
+    #endregion
+
+    #region ICartoTool
+
+    public Type UIComponent => typeof(gView.Carto.Razor.Components.Tools.Network);
+
+    async public Task<bool> OnEvent(ICartoApplicationScopeService scope, MapEvent mapEvent)
+    {
+        return true;
+    }
+
+    #endregion
+}
