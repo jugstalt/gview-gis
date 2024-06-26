dac4495d0e014656d674848a333222166b0206c0
jugstalt <gstalt@gmx.at>
2024-06-26 19:58:30 +0200

    6.24.2601

--- src/gView.Framework/Common/SystemInfo.cs
+++ src/gView.Framework/Common/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.Common
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(6, 24, 2105);
+        public static Version Version = new Version(6, 24, 2601);
 
         #region -> Private Variables
 
