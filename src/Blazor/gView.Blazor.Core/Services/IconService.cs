e5f6ad0d05a1cfff51017858c47891553c065a56
jugstalt <gstalt@gmx.at>
2024-09-24 20:13:53 +0200

    sync

--- src/Blazor/gView.Blazor.Core/Services/IconService.cs
+++ src/Blazor/gView.Blazor.Core/Services/IconService.cs
@@ -3,7 +3,7 @@
 public class IconService
 {
     public string FromString(string iconName)
-     => iconName.Contains(":") ?
+     => iconName?.Contains(":") == true ?
             $"sketchpen-icon-{iconName.Split(':')[0]}-bg-light-24 {iconName.Split(':')[1]}" :
             $"sketchpen-icon-default-bg-light-24 {iconName}";
 }
