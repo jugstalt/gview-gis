d6c54df27b76d574cd6215c8e379bb89059af2b8
jugstalt <gstalt@gmx.at>
2024-10-06 20:22:21 +0200

    sync

--- src/Blazor/gView.Blazor.Core/Services/IconService.cs
+++ src/Blazor/gView.Blazor.Core/Services/IconService.cs
@@ -10,7 +10,7 @@ public enum IconSize
 public class IconService
 {
     public string FromString(string iconName, IconSize size = IconSize.Medium)
-     => iconName.Contains(":") ?
+     => iconName?.Contains(":") == true ?
             $"sketchpen-icon-{iconName.Split(':')[0]}-bg-light-{IconSizePixels(size)} {iconName.Split(':')[1]}" :
             $"sketchpen-icon-default-bg-light-24 {IconSizePixels(size)}";
 
