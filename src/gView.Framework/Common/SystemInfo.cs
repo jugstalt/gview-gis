ca4eef22a7d75ae76aab3d66b0169a27678c345e
jugstalt <gstalt@gmx.at>
2024-11-10 20:21:48 +0100

    Release 6.24.4502

--- src/gView.Framework/Common/SystemInfo.cs
+++ src/gView.Framework/Common/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.Common
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(6, 24, 4501);
+        public static Version Version = new Version(6, 24, 4502);
 
         #region -> Private Variables
 
--- src/gView.Server/Startup.cs
+++ src/gView.Server/Startup.cs
@@ -18,7 +18,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using gView.Server.Extensions;
 
 namespace gView.Server
 {
