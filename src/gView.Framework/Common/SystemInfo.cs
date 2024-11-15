038e31c3cf3199ecc31df2af068d84d075f3859a
jugstalt <gstalt@gmx.at>
2024-11-15 07:18:19 +0100

    Carto: Bug - no editing of layernames Carto: Bug - create db connection in Explorer Dilaogs => Exception About Dialog: Link to Doc & Repo

--- src/gView.Framework/Common/SystemInfo.cs
+++ src/gView.Framework/Common/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.Common
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(6, 24, 4502);
+        public static Version Version = new Version(6, 24, 4601);
 
         #region -> Private Variables
 
