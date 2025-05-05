2c7a9eef50274a5fb9ace6da156c7cdbfee130fd
jugstalt <gstalt@gmx.at>
2025-05-05 21:09:25 +0200

    nuget update (MudBlazor)

--- src/gView.Framework/Common/SystemInfo.cs
+++ src/gView.Framework/Common/SystemInfo.cs
@@ -10,7 +10,7 @@ namespace gView.Framework.Common
 {
     public class SystemInfo
     {
-        public static Version Version = new Version(7, 25, 1801);
+        public static Version Version = new Version(7, 25, 1901);
 
         #region -> Private Variables
 
