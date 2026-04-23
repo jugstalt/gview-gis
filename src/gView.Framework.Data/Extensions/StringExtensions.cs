b0947cd775b84eb3d7fcc1d0c4b47553a06b5001
jugstalt <juergen.gstaltmaier@e-netze.at>
2026-04-23 11:51:20 +0200

    SpatialReference: MakeValid when loading map. Make it valid, old version maybe only save id/name, no parameters... MakeValid load it by id if necessary

---
+++ src/gView.Framework.Data/Extensions/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gView.Framework.Data.Extensions;
+
+internal static class StringExtensions
+{
+    public static bool IsJsonObjectOrArray(this string value)
+    {
+        value=value?.Trim();
+
+        if (string.IsNullOrEmpty(value))
+        {
+            return false;
+        }
+
+        if (value.StartsWith("[") && value.EndsWith("]"))
+        {
+            return true;
+        }
+
+        if(value.StartsWith("{") && value.EndsWith("}"))
+        {
+            return true; 
+        }
+
+        if (string.IsNullOrEmpty(value))
+        {
+            return false;
+        }
+
+        return false;
+    }
+}
